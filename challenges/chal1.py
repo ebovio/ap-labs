524ccda688628b4f27978ea061f87bc4f933df5a
Enrique <e.bovio08@gmail.com>
2019-01-18 14:08:36 -0600

    With comments

--- challenges/chal1.py
+++ challenges/chal1.py
@@ -2,17 +2,17 @@
 def test(str):
     counter = 0
     newString = [] #Creation of an empty newString to store each character
-    list = []
+    list = [] #List to store the amount of different characters
     for i in str:
         if i not in newString:
             newString.append(i) #Adding the character in the new string
             counter = counter + 1
         else:
             newString = []
-            list.append(counter)
+            list.append(counter) #Adding the value of the longest substring formed
             counter = 0
-    list.append(counter)        
-    answer = max(list)
+    list.append(counter) #Adds the last substring formed thanks to the last cycle
+    answer = max(list) #Gets the maximum value stored in the list
     print(answer)
 
 #Main Program
