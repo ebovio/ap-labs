b92f28e3550d09c0f513a0954b1096aea6cbb14a
Enrique <e.bovio08@gmail.com>
2019-02-05 15:56:14 -0600

    lab 3

---
+++ labs/lab0.3/exercise-methods.go
@@ -0,0 +1,59 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 156.
+
+// Package geometry defines simple types for plane geometry.
+//!+point
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+type Point struct{ x, y float64 }
+
+func (p Point) X() float64 {
+	return p.x
+}
+
+func (p Point) Y() float64 {
+	return p.y
+}
+
+func printPoint(p Point) {
+	fmt.Printf("%f, %f\n", p.X(), p.Y())
+}
+
+// traditional function
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X()-p.X(), q.Y()-p.Y())
+}
+
+// same thing, but as a method of the Point type
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X()-p.X(), q.Y()-p.Y())
+}
+
+
+// A Path is a journey connecting the points with straight lines.
+type Path []Point
+
+// Distance returns the distance traveled along the path.
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
+
+//!-path
+
+func main() {
+	var p = Point{8, 11}
+	printPoint(p)
+}
