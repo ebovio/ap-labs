5abdc3e3584ee692e6ff316015f780461430c47f
Enrique <e.bovio08@gmail.com>
2019-02-15 12:15:24 -0600

    Ejercicio go

---
+++ challenges/chal2/dir-scan.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func scanDir(dir string) error {
+
+	directories := 0
+	symbolicLinks := 0
+	other := 0
+
+	// Scan
+	filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
+		if f.IsDir() {
+			directories++
+		}
+		if f.Mode()&os.ModeSymlink != 0 {
+			symbolicLinks++
+		}
+
+		if f.Mode().IsRegular() {
+			other++
+		}
+		return nil
+	})
+	fmt.Printf("Path: %s\n", dir)
+	fmt.Printf("Directories: %d\n", directories)
+	fmt.Printf("Symbolic Links: %d\n", symbolicLinks)
+	fmt.Printf("Other Files: %d\n", other)
+	return nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ./dir-scan <directory>")
+		os.Exit(1)
+	}
+	directoryPath := os.Args[1]
+	scanDir(directoryPath)
+}
