c45eb6865866bf63ef4c7cf107531063ecb9aa59
Enrique <e.bovio08@gmail.com>
2019-03-12 17:54:09 -0600

    lab 2.4

---
+++ labs/lab2.4/testLogger.c
@@ -0,0 +1,32 @@
+int initLogger (char *logType);
+int infof(const char *format, ...);
+int warnf(const char *format, ...);
+int errorf(const char *format, ...);
+int panicf(const char *format, ...);
+void textcolor(int attr, int fg);
+
+int main(){
+	infof("This is general information.\n");
+	warnf("This is a warning.\n");
+	errorf("Holy! This is an error.\n");
+	panicf("Don't panic! Non recoverable issues with core dump.\n");
+
+	printf("\n");
+
+	initLogger("stdout");   
+	infof("This is general information.\n");
+	warnf("This is a warning.\n");
+	errorf("Holy! This is an error.\n");
+	panicf("Don't panic! Non recoverable issues with core dump.\n");
+
+	printf("\n");
+	printf("Check your syslog file at /var/log/syslog to see the rest of calls\n");
+	initLogger("syslog");   
+	infof("[info]\n");
+	warnf("[warn] %s \n", "warn");
+	errorf("[error] %s %s \n", "error", "test");
+   	panicf("[panic]\n");
+
+	return 0;
+}
+
